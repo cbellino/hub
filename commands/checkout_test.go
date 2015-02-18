6e0d23d6199283d14ac1132d58ed3fcd9db41b26
Jingwen Owen Ou <jingweno@gmail.com>
2015-02-17 22:27:15 -0800

    Report error when there're `-b` or `--orphan` flags in checkout pull request

--- commands/checkout_test.go
+++ commands/checkout_test.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/github/hub/Godeps/_workspace/src/github.com/bmizerany/assert"
@@ -8,9 +9,21 @@ import (
 
 func TestReplaceCheckoutParam(t *testing.T) {
 	checkoutURL := "https://github.com/github/hub/pull/12"
-	args := NewArgs([]string{"checkout", "-b", checkoutURL})
+	args := NewArgs([]string{"checkout", checkoutURL})
 	replaceCheckoutParam(args, checkoutURL, "jingweno", "origin/master")
 
 	cmd := args.ToCmd()
-	assert.Equal(t, "git checkout -b --track -B jingweno origin/master", cmd.String())
+	assert.Equal(t, "git checkout --track -B jingweno origin/master", cmd.String())
+}
+
+func TestTransformCheckoutArgs(t *testing.T) {
+	args := NewArgs([]string{"checkout", "-b", "https://github.com/github/hub/pull/12"})
+	err := transformCheckoutArgs(args)
+
+	assert.Equal(t, "Unsupported flag -b when checking out pull request", fmt.Sprintf("%s", err))
+
+	args = NewArgs([]string{"checkout", "--orphan", "https://github.com/github/hub/pull/12"})
+	err = transformCheckoutArgs(args)
+
+	assert.Equal(t, "Unsupported flag --orphan when checking out pull request", fmt.Sprintf("%s", err))
 }
